b4d28717dc575d4d25d3fd11b3bcb31728990b4d
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2020-12-28 18:06:25 +0100

    Navbar component added

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Nuxt SSR</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
