c5ae6c1167978729a0d1ffb408ef8b859e9abbe6
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-03 16:28:17 +0100

    data fetching

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,14 +2,21 @@
   <section>
     <h1>Users page</h1>
     <ul>
-      <li v-for="user of 5" :key="user">
-        <a href="#" @click.prevent="openUser(user)">User {{ user }}</a>
+      <li v-for="user of users" :key="user">
+        <a href="#" @click.prevent="openUser(user)"> {{ user.name }}</a>
       </li>
     </ul>
   </section>
 </template>
 <script>
 export default {
+  async asyncData ({ $axios }) {
+    const users = await $axios.$get('https://jsonplaceholder.typicode.com/users')
+    return { users }
+  },
+  data: () => ({
+    users: []
+  }),
   methods: {
     openUser (user) {
       this.$router.push('/users/' + user)
